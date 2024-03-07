0aedc42f6b3d2ebe013130c534606e1bf4cf3ccb
lucascomin9 <126088114+lucascomin9@users.noreply.github.com>
2024-03-06 21:07:34 -0300

    Add files via upload

---
+++ 1_tipos_primitivos/1_number/script.js
@@ -0,0 +1,17 @@
+    //tipo number
+
+    const meuNumero = 13;
+
+    const telefone = 44988131171;
+
+    const idade = 16;
+
+
+    console.log(meuNumero);
+    console.log(telefone);
+    console.log(idade);
+
+
+    const soma = meuNumero + idade;
+
+    console.log(soma)
---
+++ 1_tipos_primitivos/2_string/script.js
@@ -0,0 +1,16 @@
+const texto1 = "Tomas Turbando";
+const texto2 = "Xupa Seios";
+
+const texto3 = 'O Xupa Seios foi no "Banheiro"';
+
+console.log(texto1);
+console.log(texto2);
+console.log(texto3);
+
+console.log(texto2 + texto3);
+
+const cifrao = '\u0024';
+const aMaiusculo = '\u0041';
+
+console.log(cifrao);
+console.log(aMaiusculo);
