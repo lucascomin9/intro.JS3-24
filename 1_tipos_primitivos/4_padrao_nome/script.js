ce2dd9c50ec094dd47e9f5499df332c52db8952a
lucascomin9 <126088114+lucascomin9@users.noreply.github.com>
2024-03-25 20:19:46 -0300

    Add files via upload

---
+++ 1_tipos_primitivos/4_padrao_nome/script.js
@@ -0,0 +1,14 @@
+    const minhaVar = 1;
+
+    const MinhaVar = 'Texto';
+
+    const minhavar = '3';
+
+    const MINHAVAR = 2;
+
+    console.log(minhaVar, MinhaVar, minhavar, MINHAVAR)
+
+
+
+
+    
---
+++ 2_declarando_variaveis/1_var_let_const/script.js
@@ -0,0 +1,26 @@
+var altura = 5;
+var comprimento = 7;
+
+area = altura * comprimento;
+
+console.log(area);
+
+/*let só permite que a variável seja utilizada após sua declaração.*/
+
+if(forma === 'retângulo'){
+    area = altura2 * comprimento2;
+}else{
+    area = (altura2 * comprimento2)/2;
+}
+console.log(area);
+// let area;
+
+/* A const é uma constante, ou seja, uma vez que a colocamos no programa não conseguimos alterá-la.*/
+
+const forma3 = 'quadrado';
+const altura3 = 5;
+const comprimento3 = 7;
+
+const area3 = altura3 * comprimento3;
+
+console.log(area3);
